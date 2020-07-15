1422a2ee1461117724ee43b3ef4562f98cbaaff1
VamsiSaiTumu <68236966+VamsiSaiTumu@users.noreply.github.com>
2020-07-15 21:13:05 +0530

    Add files via upload

---
+++ subtraction.py
@@ -0,0 +1,12 @@
+print("subtraction of two numbers")
+print(" ")
+number1= int(input("please enter the first number:"))
+number2= int(input("please enter the second number:"))
+if number1>number2:
+    print(" ")
+    print("\n number1 is greater than number2 so:")
+    print(f"the subtraction of {number1} and {number2} is:",number1-number2)
+else:
+    print(" ")
+    print("number2 is greater than number1 so:")
+    print(f"the subtraction of {number2} and {number1} is:",number2-number1)
